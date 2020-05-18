20a651fb4393c6f5f1bb0cc77c098e282a53e28d
Mario De Donno <mamidd@gmail.com>
2020-05-18 20:43:39 +0200

    Re-arrange structure folder

--- 4-project/1-homework/src/main.js
+++ 4-project/1-homework/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Header from './Header.vue'
-import ServersManagment from './ServersManagment.vue'
-import ServerList from './ServerList.vue'
+import Header from './components/Header.vue'
+import ServersManagment from './components/ServersManagment.vue'
+import ServerList from './components/ServerList.vue'
 
 Vue.component('app-header', Header)
 Vue.component('app-servers-managment', ServersManagment)
