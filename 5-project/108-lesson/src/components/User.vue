31b7ecd422e3cb37301647011460a2ed57489032
Mario De Donno <mamidd@gmail.com>
2020-05-19 19:22:02 +0200

    Add 5 project folder

---
+++ 5-project/108-lesson/src/components/User.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <p>User Name: {{name}}</p>
+        <button @click="changeName">Change My Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                :name="name" 
+                :userAge="age"
+                @nameWasReset="name = $event"
+                :resetFn="resetName"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                :userAge="age"
+                @ageEdited="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return {
+                name: 'Mario',
+                age: 27
+            }
+        },
+        methods: {
+          changeName() {
+              this.name = 'Anna'
+          },
+          resetName() {
+              this.name = "Resetted"
+          }  
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ 5-project/108-lesson/src/components/UserDetail.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+        <p>Reverse User Name: {{ switchName() }}</p>
+        <p>Age: {{ userAge }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name Fn</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String
+            },
+            userAge: {
+                type: Number
+            },
+            resetFn: {
+                type: Function
+            }
+        },
+        methods: {
+            switchName() {
+                return this.name.split("").reverse().join("")
+            },
+            resetName() {
+                this.name = 'Mario'
+                this.$emit('nameWasReset', this.name)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
