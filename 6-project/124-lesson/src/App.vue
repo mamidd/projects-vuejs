d77e15494bd53e969ec9e713c9687b8dbfd5aedc
Mario De Donno <mamidd@gmail.com>
2020-05-22 19:46:43 +0200

    Complete project 6

--- 6-project/124-lesson/src/App.vue
+++ 6-project/124-lesson/src/App.vue
@@ -2,6 +2,15 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
+                <template>
+                    <button @click="selectedComponent = 'app-quote'">Quote</button>
+                    <button @click="selectedComponent = 'app-author'">Author</button>
+                    <button @click="selectedComponent = 'app-new'">New</button>
+                    <p>{{ selectedComponent }}</p>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </template>
                <app-quote>
                    <h2>Sono un h2</h2>
                    <p>Sono un paragrafo</p>
@@ -18,16 +27,21 @@
 
 <script>
     import quote from './components/Quote.vue'
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
     import quote2 from './components/Quote2.vue'
     export default {
         data: function(){
             return {
-                text: "Sono un h3"
+                text: "Sono un h3",
+                selectedComponent: 'app-quote'
             }
         },
         components:{
             'app-quote': quote,
-            'app-quote-bis': quote2
+            'app-quote-bis': quote2,
+            'app-author': Author,
+            'app-new': New
         }
     }
 </script>
