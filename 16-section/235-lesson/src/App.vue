b9bc97d3e00c5cccdd5549e155f9d42b2d2cf384
Mario De Donno <mamidd@gmail.com>
2020-06-26 19:12:39 +0200

    Add section 16

---
+++ 16-section/235-lesson/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+                <hr>
+                <app-header></app-header>
+                <app-header-bis></app-header-bis>
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue'
+    import Header2 from './components/Header2.vue'
+    export default {
+        components:{
+            appHeader: Header,
+            appHeaderBis: Header2
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ 16-section/244-lesson/src/routes.js
@@ -0,0 +1,21 @@
+import User from './components/user/User.vue'
+import UserStart from './components/user/UserStart.vue'
+import UserDetail from './components/user/UserDetail.vue'
+import UserEdit from './components/user/UserEdit.vue'
+import Home from './components/Home.vue'
+import Header from './components/Header.vue'
+
+export const routes = [
+    { path: '', components: {
+        default: Home,
+        'header-top': Header
+    }},
+    { path: '/user', components: {
+        default: User,
+        'header-bottom': Header
+    }, children: [
+        { path: '', component: UserStart },
+        { path: ':id', component: UserDetail, name:'userDetail', props: true },
+        { path: ':id/edit', component: UserEdit, name: 'userEdit' },
+    ]},
+];
