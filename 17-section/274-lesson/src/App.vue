bd844130b327e0b3b39b66c1af224ae3391c1bb9
Mario De Donno <mamidd@gmail.com>
2020-07-17 20:07:54 +0200

    Finish section 17

---
+++ 17-section/274-lesson/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Vuex</h1>
+                <app-result></app-result>
+                <app-another-result></app-another-result>
+                <hr>
+                <app-another-counter></app-another-counter>
+                <app-async-counter></app-async-counter>
+                <hr>
+                <input type="text" v-model="value">
+                <p>{{ value }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import AnotherCounter from './components/AnotherCounter.vue';
+    import AsyncCounter from './components/AsyncCounter.vue';
+    import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
+
+    export default {
+        computed: {
+            value: {
+                get() {
+                    return this.$store.getters.value;
+                },
+                set(value) {
+                    this.$store.dispatch('updateValue', value);
+                }
+            }
+        },
+        components: {
+            appAnotherCounter: AnotherCounter,
+            appAsyncCounter: AsyncCounter,
+            appResult: Result,
+            appAnotherResult: AnotherResult,
+        }
+    }
+</script>
+
---
+++ 17-section/274-lesson/src/components/AnotherResult.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <!-- <p>Counter is: {{ counter }}</p>
+        <p>Number of Clicks: {{ clicks }}</p> -->
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of Clicks: {{ stringCounter }}</p>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    import * as types from '../store/types'
+    export default {
+        // computed: {
+        //     counter() {
+        //         return this.$store.getters.doubleCounter;
+        //     },
+        //     clicks() {
+        //         return this.$store.getters.stringCounter;
+        //     }
+        // }
+        computed: {
+            ...mapGetters({
+                doubleCounter: types.DOUBLE_COUNTER,
+                stringCounter: types.CLICK_COUNTER,
+            })
+        }
+    }
+</script>
